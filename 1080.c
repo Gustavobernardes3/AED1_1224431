88b59dc925fc7dd0b1730ff6066c1912315e42a5
Gustavobernardes3 <gustavo.bernardes@unifesp.br>
2026-08-23 15:51:07 -0300

    Enhance comments in maximo function and main
    Added comments for clarity and improved readability.

--- 1080.c
+++ 1080.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
+/* Retorna o maior valor entre os n primeiros elementos */
 int maximo(int v[], int n) {
 
+    /* Caso base: apenas 1 elemento */
     if (n == 1) {
         return v[0];
     }
     else {
+
         int x;
+
+        /* Descobre o maior dos n-1 elementos */
         x = maximo(v, n - 1);
 
+        /* Compara o maior anterior com o último elemento */
         if (x > v[n - 1]) {
             return x;
         }
@@ -22,19 +28,23 @@ int main() {
 
     int v[100], maior, posicao;
 
+    /* Lê os 100 valores */
     for (int i = 0; i < 100; i++) {
         scanf("%d", &v[i]);
     }
 
+    /* Encontra o maior valor usando recursão */
     maior = maximo(v, 100);
 
+    /* Procura a posição do maior */
     for (int i = 0; i < 100; i++) {
         if (v[i] == maior) {
-            posicao = i + 1;
+            posicao = i + 1; // +1 porque posição começa em 1
             break;
         }
     }
 
+    /* Imprime maior valor e sua posição */
     printf("%d\n", maior);
     printf("%d\n", posicao);
 
